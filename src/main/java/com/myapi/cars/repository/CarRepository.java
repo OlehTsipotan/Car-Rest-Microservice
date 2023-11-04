cbde2d048c30a30b418e41c39b1a03b30ec74bd2
olehtsipotan <oleh.tsipotan@gmail.com>
2023-11-04 22:52:34 +0200

    CarRepository + Test

---
+++ src/main/java/com/myapi/cars/repository/CarRepository.java
@@ -0,0 +1,22 @@
+package com.myapi.cars.repository;
+
+import com.myapi.cars.model.Car;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CarRepository extends JpaRepository<Car, UUID> {
+
+    @Query("SELECT c FROM Car c WHERE " +
+            "(?1 IS NULL OR c.make.name = ?1)" +
+            "AND (?2 IS NULL OR c.year = ?2) " +
+            "AND (?3 IS NULL OR c.model = ?3) " +
+            "AND (?4 IS NULL OR ?4 IN (SELECT cat.name FROM c.categories cat))")
+    Page<Car> findAll(String makeName, Integer year, String model, String categoryName, Pageable pageable);
+
+}
