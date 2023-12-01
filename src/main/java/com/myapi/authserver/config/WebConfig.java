1058fdea9c5007e842610fad78c1fe3393a2c755
Oleh Tsipotan <tsipotan.oleh@gmail.com>
2023-12-02 00:25:21 +0200

    read & connection timeout config

--- src/main/java/com/myapi/authserver/config/WebConfig.java
+++ src/main/java/com/myapi/authserver/config/WebConfig.java
@@ -4,9 +4,12 @@ import com.myapi.authserver.client.Auth0Client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.time.Duration;
 
 @Configuration
 public class WebConfig {
@@ -14,9 +17,26 @@ public class WebConfig {
     @Value("${auth0.base-url}")
     private String baseUrl;
 
+    @Value("${client.connection-timeout}")
+    private int connectTimeout;
+
+    @Value("${client.read-timeout}")
+    private int readTimeout;
+
     @Bean
-    RestClient restClient(RestClient.Builder builder) {
-        return builder.baseUrl(baseUrl).build();
+    RestClient restClient(SimpleClientHttpRequestFactory simpleClientHttpRequestFactory) {
+        return RestClient.builder()
+                .requestFactory(simpleClientHttpRequestFactory)
+                .baseUrl(baseUrl)
+                .build();
+    }
+
+    @Bean
+    SimpleClientHttpRequestFactory simpleClientHttpRequestFactory() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(Duration.ofSeconds(connectTimeout));
+        factory.setReadTimeout(Duration.ofSeconds(readTimeout));
+        return factory;
     }
 
     @Bean
