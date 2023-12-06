31e3f5fc06ad678ceb6f8f91fc5507f215462584
Oleh Tsipotan <tsipotan.oleh@gmail.com>
2023-12-06 17:11:14 +0200

    @NonNull in convert method

--- src/main/java/com/myapi/authserver/converter/PasswordAuthenticationRequestBodyFromUserCredentialsConverter.java
+++ src/main/java/com/myapi/authserver/converter/PasswordAuthenticationRequestBodyFromUserCredentialsConverter.java
@@ -2,6 +2,7 @@ package com.myapi.authserver.converter;
 
 import com.myapi.authserver.model.PasswordAuthenticationRequestBody;
 import com.myapi.authserver.model.UserCredentials;
+import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -9,19 +10,19 @@ import org.springframework.stereotype.Component;
 public class PasswordAuthenticationRequestBodyFromUserCredentialsConverter {
 
     @Value("password")
-    private String grant_type;
+    private String grantType;
 
     @Value("${auth0.audience}")
     private String audience;
 
     @Value("${auth0.client.id}")
-    private String client_id;
+    private String clientId;
 
     @Value("${auth0.client.secret}")
-    private String client_secret;
+    private String clientSecret;
 
-    public PasswordAuthenticationRequestBody convert(UserCredentials userCredentials) {
-        return new PasswordAuthenticationRequestBody(grant_type, userCredentials.username(), userCredentials.password(),
-                audience, client_id, client_secret);
+    public PasswordAuthenticationRequestBody convert(@NonNull UserCredentials userCredentials) {
+        return new PasswordAuthenticationRequestBody(grantType, userCredentials.username(), userCredentials.password(),
+                audience, clientId, clientSecret);
     }
 }
